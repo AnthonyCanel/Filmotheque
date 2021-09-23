488b19611f19147cad5248857112e032820db4ab
AnthonyCanel <canelanthony@gmail.com>
2021-09-23 12:31:36 +0200

    first commit

---
+++ src/main/java/fr/eni/demoInjectionDependance/Filmotheque2Application.java
@@ -0,0 +1,13 @@
+package fr.eni.demoInjectionDependance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Filmotheque2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Filmotheque2Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/demoInjectionDependance/Filmotheque2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.demoInjectionDependance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Filmotheque2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
