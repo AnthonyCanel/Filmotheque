88702eed445a20ebe00c94f2d4ad555cdcdef8da
cptGouZ <TFtRSN7tnnix>
2021-09-28 10:23:11 +0200

    Ajout lombok

--- src/main/java/fr/eni/demoInjectionDependance/bo/Participant.java
+++ src/main/java/fr/eni/demoInjectionDependance/bo/Participant.java
@@ -1,47 +1,27 @@
 package fr.eni.demoInjectionDependance.bo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Participant {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private String nom;
 	private String prenom;
 	//contructeurs
-	public Participant() {
-		
-	}
 	public Participant(String nom, String prenom) {
-		
 		this.nom = nom;
 		this.prenom = prenom;
 	}
-	public Participant(long id, String nom, String prenom) {
-		
-		this.id = id;
-		this.nom = nom;
-		this.prenom = prenom;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	
-	//Getter/setter
-	
-	
-	
-	
 }
