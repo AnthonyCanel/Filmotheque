c355c92388782b52e2ea0146f4dcfa7a2472764d
cptGouZ <TFtRSN7tnnix>
2021-09-28 13:37:36 +0200

    Ajout Id sur BO Genre Démarrage application Ok

--- src/main/java/fr/eni/demoInjectionDependance/bo/Genre.java
+++ src/main/java/fr/eni/demoInjectionDependance/bo/Genre.java
@@ -5,12 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Genre {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private String libelle;
 
